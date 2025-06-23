e38f99d52d9177cd16b062c92a727b9eccdbac80
Keith Burzinski <kbx81x@gmail.com>
2025-06-23 00:17:34 -0500

    Add some example I/O

--- components/empty_spi_sensor/empty_spi_sensor.cpp
+++ components/empty_spi_sensor/empty_spi_sensor.cpp
@@ -7,19 +7,32 @@ namespace empty_spi_sensor {
 static const char *TAG = "empty_spi_sensor.sensor";
 
 void EmptySPISensor::setup() {
+  // SPI device initialization is typically done here.
+  // Note that a number of read/write methods are available in the SPIDevice
+  // class. See "spi/spi.h" for details.
+  this->spi_setup(); // Required to initialize this SPI device
 
+  this->enable();
+  uint8_t initialize_cmd = 0x12; // Example command to initialize the device
+  this->write_byte(initialize_cmd);
+
+  uint8_t response = read_byte(); // Read the response from the device
+  this->disable();
+
+  if (response != 0) { // Example check for a specific response
+    ESP_LOGE(TAG, "Initialization failed; response: %d", response);
+    this->mark_failed(); // Mark the component as failed if the response is not
+                         // as expected
+    return;
+  }
 }
 
 void EmptySPISensor::update() {
-
+  // Work to be done at each update interval
 }
 
-void EmptySPISensor::loop() {
-
-}
-
-void EmptySPISensor::dump_config(){
-    ESP_LOGCONFIG(TAG, "Empty SPI sensor");
+void EmptySPISensor::dump_config() {
+  ESP_LOGCONFIG(TAG, "Empty SPI sensor");
 }
 
 }  // namespace empty_spi_sensor
